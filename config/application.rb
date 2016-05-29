089aab451b666700c56d2cdbd1c8996b0aff5c86
Omair <omair@gems.techverx.com>
2016-05-29 09:33:00 +0500

    heroku issue

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module RubyGettingStarted
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
    config.assets.enabled = true
+   config.serve_static_files = true 
   end
 end
